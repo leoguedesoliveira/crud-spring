9dbbe38d156ff8b8b19978bc5cd39074529685a0
LeoGuedes <leonardoguedes.dasilvaguedesol@gmail.com>
2023-08-18 23:49:20 -0300

    Commit unversioned files

---
+++ crud-spring/src/test/java/com/lgtech/CrudSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lgtech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
